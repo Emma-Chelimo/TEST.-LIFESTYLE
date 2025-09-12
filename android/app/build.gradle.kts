cd6f14cadc4d44cb8bd81b6b8ca1fc869a1ea6ef
Emma <emmachelimo21@gmail.com>
2025-09-12 16:04:23 +0300

    changed the ndk to 27.13

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,7 +13,8 @@ plugins {
 android {
     namespace = "com.example.project"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.3.13750724"
+    
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
